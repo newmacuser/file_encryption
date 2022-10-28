572f1a14e11705dee0f97ab6090e631c99a2223c
Tianming Yao <ytm1234@live.com>
2022-10-28 16:38:43 -0400

    Update encryption1.1.sh

--- encryption1.1.sh
+++ encryption1.1.sh
@@ -4,7 +4,7 @@ usage(){
 Usage:
   -i,    input file
   -p,    password
-  -n,    iterations of encryption/decryption, not to exceed 10
+  -n,    iterations [int 1-10], default 1
   -o,    output file
   -d,    decryption, default encryption
   -h,    display this help and exit
@@ -13,11 +13,13 @@ Usage:
 }
 decryption="false"; declare -i number=1
 while getopts "i:p:n:odh" arg; do
-    case $arg in
+    case "${arg}" in
       i)
-         input="$OPTARG";;
+         input="$OPTARG" 
+	 echo "Your input file: $input";;
       p)
-         passwd="$OPTARG";;
+         passwd="$OPTARG"
+	 echo "Password is: $passwd";;
       n)
          declare -i number="$OPTARG";;
       o)
@@ -32,27 +34,28 @@ while getopts "i:p:n:odh" arg; do
          decryption="true";;
       h)
          usage; exit;;
-      ?)
-         echo "Invalid option or argument" >&2
+      *)
+         echo "Invalid option or argument"
 	   usage; exit 1;;
     esac
 done
+if [ -z "${input}" ] || [ -z "${passwd}" ]; then
+    echo "
+Error: Input file and password are required."
+    usage; exit 1
+fi
 if [ $number -gt 10 ] || [ $number -lt 1 ]; then
 	echo "Invalid: The iteration number is larger than 10 or smaller than 1."; exit 1
 else
  if [ -z "$output" ]; then
 	 output="$passwd"-"$number".txt
- 	 echo "$input"
-	 echo "$passwd"
-	 echo "$number"
-	 echo "$output"
- 	 echo "$decryption"
+	 echo "Iteration: $number"
+	 echo "Output file: $output"
+ 	 echo "Decryption: $decryption"
  else
-	 echo "$input"
-	 echo "$passwd"
-	 echo "$number"
-	 echo "$output"
- 	 echo "$decryption"
+	 echo "Iteration: $number"
+	 echo "Output file: $output"
+ 	 echo "Decryption: $decryption"
  fi
  if [ $decryption == "false" ]
  then
