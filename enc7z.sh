08e02a7db94155249a8e7cdd94a2f25558e6339d
Tianming Yao <ytm1234@live.com>
2023-06-23 20:47:00 -0400

    Update and rename encz.sh to enc7z.sh

--- enc7z.sh
+++ enc7z.sh
@@ -12,7 +12,7 @@ Usage:
   example: ./encz.sh -i input.txt -p password -n 3 -o output.txt
 "
 }
-decryption="false"; declare -i number=1
+decryption="false"; declare -i number=1; passwd="MDAxMA=="
 while getopts "i:p:n:odh" arg; do
     case "${arg}" in
       i)
@@ -40,9 +40,9 @@ while getopts "i:p:n:odh" arg; do
 	   usage; exit 1;;
     esac
 done
-if [ -z "${input}" ] || [ -z "${passwd}" ]; then
+if [ -z "${input}" ] ; then
     echo "
-Error: Input file and password are required."
+Error: Input file name is required."
     usage; exit 1
 fi
 if [ $number -gt 10 ] || [ $number -lt 1 ]; then
@@ -62,7 +62,7 @@ else
  then
    if [ $number -eq 1 ]; then
        cat "$input" | openssl enc -aes-256-cbc -md sha384 -a -e -pass pass:"$passwd" -nosalt -out "$output"
-       7z a -t7z -r "$output".7z "$output" -mx9 -m0=LZMA2:d=29:fb=256 -ms=200m -mmt2 && rm "$output"
+       7z a -t7z -r "$input"_"$output".7z "$output" -mx9 -m0=LZMA2:d=29:fb=256 -ms=200m -mmt2 && rm "$output"
    else
        x=1
 	 cat "$input" | openssl enc -aes-256-cbc -md sha384 -a -e -pass pass:"$passwd" -nosalt -out temp_"$x".txt
@@ -71,21 +71,24 @@ else
 		 x=$(( $x + 1 ))
 	 done
 	 mv temp_"$x".txt "$output"; rm temp_*.txt
-	 7z a -t7z -r "$output".7z "$output" -mx9 -m0=LZMA2:d=29:fb=256 -ms=200m -mmt2 && rm "$output"
+	 7z a -t7z -r "$input"_"$output".7z "$output" -mx9 -m0=LZMA2:d=29:fb=256 -ms=200m -mmt2 && rm "$output"
    fi
  else
+   ext=$(sed 's/.*\.\(.*\)_.*/\1/' <<< "$input")
+   iname=$(sed 's/.*_\(.*\)\..*/\1/' <<< "$input")
+   oname="${input%%.*}"
    if [ $number -eq 1 ]; then
        7z x "$input"
-       cat ${input%.*} | openssl enc -aes-256-cbc -md sha384 -a -d -pass pass:"$passwd" -nosalt > "$output" && rm ${input%.*}
+       cat "$iname" | openssl enc -aes-256-cbc -md sha384 -a -d -pass pass:"$passwd" -nosalt > "$oname".de."$ext" && rm "$iname"
    else
        x=1
      7z x "$input"
-	 cat ${input%.*} | openssl enc -aes-256-cbc -md sha384 -a -d -pass pass:"$passwd" -nosalt > temp_"$x".txt && rm ${input%.*}
+	 cat "$iname" | openssl enc -aes-256-cbc -md sha384 -a -d -pass pass:"$passwd" -nosalt > temp_"$x".txt && rm "$iname"
 	 while [ $x -lt $number ]; do
 		 cat temp_"$x".txt | openssl enc -aes-256-cbc -md sha384 -a -d -pass pass:"$passwd" -nosalt > temp_"$(( $x + 1 ))".txt
              x=$(( $x + 1 ))
 	 done
-	 mv temp_"$x".txt "$output"; rm temp_*.txt
+	 mv temp_"$x".txt "$oname".de."$ext" && rm temp_*.txt
    fi
  fi
 fi
